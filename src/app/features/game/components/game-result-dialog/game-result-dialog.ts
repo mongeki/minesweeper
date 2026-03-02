c5ca7351a2a47fbdf2b8848759e9cd87015ac63b
mongeki <mongeki@mail.ru>
2026-03-02 21:30:45 +0700

    add dialog imports

--- src/app/features/game/components/game-result-dialog/game-result-dialog.ts
+++ src/app/features/game/components/game-result-dialog/game-result-dialog.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 import { GameStatus } from '../../../../domain/enums/game-status.enum';
 
 @Component({
   selector: 'app-game-result-dialog',
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule],
   templateUrl: './game-result-dialog.html',
   styleUrl: './game-result-dialog.scss',
 })
